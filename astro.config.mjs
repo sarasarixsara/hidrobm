94018933c3cf97fa646b3ce53fca7f2f3101c6c6
sarasarixsara <sara.cuy.ramirez@gmail.com>
2025-01-28 00:27:41 -0500

    changed base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: "https://vbartalis.github.io",
-  base: "/energy-template"
+  base: "/"
 });
